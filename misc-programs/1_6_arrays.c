be510b436c1010266d74ad74b0e0bfcaf1fbd85a
David Banks <amoe@solasistim.net>
2024-05-24 07:42:15 +0100

    arrays program

--- misc-programs/1_6_arrays.c
+++ misc-programs/1_6_arrays.c
@@ -4,8 +4,8 @@ int main() {
     int c;
     int i;
     
-    /*int whitespace_count;
-    int other_count;*/
+    int whitespace_count = 0;
+    int other_count = 0;
     int digit_count[10];
 
     /* Initialize the digit count array. */
@@ -17,6 +17,10 @@ int main() {
         if (c >= '0' && c <= '9') {
             int digit_index = c - '0';
             ++digit_count[digit_index];
+        } else if (c == ' ' || c == '\n' || c == '\t') {
+            ++whitespace_count;
+        } else {
+            ++other_count;
         }
     }
 
@@ -24,5 +28,5 @@ int main() {
     for (i = 0; i < 10; i++) {
         printf(" %d", digit_count[i]);
     }
-    putchar('\n');
+    printf(", white space = %d, other = %d\n", whitespace_count, other_count);
 }
