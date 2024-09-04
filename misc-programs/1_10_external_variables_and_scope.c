11a1d93f182e767e12bd9af944e94edd5098811e
Dave Banks <dave.banks@15gifts.com>
2024-09-04 16:21:08 +0100

    add new program

---
+++ misc-programs/1_10_external_variables_and_scope.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MAXIMUM_LINE_LENGTH 128
+
+int max;
+char line[MAXIMUM_LINE_LENGTH];
+char longest[MAXIMUM_LINE_LENGTH];
+
+int main() {
+    
+    while (getline()) {
+        
+    }
+
+    
+    printf("Longest line:\n");
+    printf("The length of the longest line was: %d\n");
+    
+    return 0;
+}
