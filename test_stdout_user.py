0ad126f5cf1a5df07dc2a217945a3139521b76b1
David Banks <amoe@solasistim.net>
2024-11-21 20:50:55 +0000

    test cases for entab program

--- test_stdout_user.py
+++ test_stdout_user.py
@@ -1,13 +1,25 @@
 import subprocess
 
 test_data = [
-    (b'foobar\n', b'foobar\n')
+    (b'foobar\n', b'foobar\n'),
+    (b'foo     \n', b'foo\t\n'),
+    (b'foobar00foo     \n', b'foobar00foo\t\n'),
+    (b'foobar   \n', b'foobar\t\n'),
+    (b'foo bar baz quux\n', b'foo bar\tbaz quux\n'),
+    (b'foobar0 ', b'foobar0\t'),
+    (b'foobar00foobar0 ', b'foobar00foobar0\t\n'),
+    (b'foobar00 oobar0 \n', b'foobar00 oobar0\t\n'),
+    (
+        b'abcdefghabcd                        efghabcdefgh\n',
+        'abcdefghabcd\t\t\t    efghabcdefgh\n'
+    )
 ]
 
 
 success = 0
 
 print("Running test suite.")
+
 
 for test_input, expected in test_data:
     process = subprocess.run(
@@ -16,7 +28,7 @@ for test_input, expected in test_data:
     result = process.stdout
 
     if result != expected:
-        raise Exception(f'Failure: {result!r} was not {expected!r}')
+        raise Exception(f'Test failure:\nActual output {result!r} was not {expected!r}')
     else:
         success += 1
     
