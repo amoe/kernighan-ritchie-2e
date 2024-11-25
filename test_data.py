8dfddb6d49a7605187dc2273c24b34190aa6aa8e
David Banks <amoe@solasistim.net>
2024-11-25 08:22:39 +0000

    clarifying test requirements

--- test_data.py
+++ test_data.py
@@ -19,4 +19,23 @@ def ex_1_21():
 def ex_1_22():
     return [
         (b'foobar\n', b'foobar\n'),
+
+        # The happy path.
+        (b'foo bar baz\n', b'foo bar\nbaz\n'),
+
+        # We should only output a trailing newline if the trailing newline was
+        # already present in the input.
+        (b'foo bar baz', b'foo bar\nbaz'),
+
+        # 'Very long lines' means presumably exceeding a hardcoded buffer size.
+        # Our buffer size is only going to be 128 bytes.
+        # Can't we just support unlimited length?
+        # Something like:
+#        (b' '.join([b'foo bar baz'] * 12), 
+
+
+        # Something intelligent is not clear in this case but it makes sense that
+        # we can only really break the line at the Nth column as we don't have
+        # any info about the structure of the text.
+        (b'foobarbaz\n', b'foobarba\nz\n')
     ]
